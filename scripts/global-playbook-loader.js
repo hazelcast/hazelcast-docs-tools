77b4b4dd7de9cf4af89a0df66e6fc9a9156fa423
Mykola Fant <mykola.fant@hazelcast.com>
2025-03-17 11:16:59 +0200

    add docs-ui loader

--- scripts/global-playbook-loader.js
+++ scripts/global-playbook-loader.js
@@ -1,17 +1,32 @@
-const YAML = require('yaml');
-const { isMatch } = require('matcher');
-const fs = require('fs');
-const {parseArgs} = require('node:util');
+const { parseArgs} = require('node:util');
+const { PlaybookLoaderUtils } = require('./playbook-loader-utils');
 
 
 class PlaybookLoader {
-	static async fetchGlobalAntoraPlaybook() {
-		const response = await fetch('https://api.github.com/repos/hazelcast/hazelcast-docs/contents/antora-playbook.yml');
-		const playbook = await response.json();
-		const globalAntoraPlaybookContent = Buffer.from(playbook.content, 'base64').toString('utf-8');
-		return YAML.parse(globalAntoraPlaybookContent);
-	}
-
+	static INPUT_ARGS_OPTIONS = {
+		repo: {
+			type: 'string',
+			short: 'r',
+		},
+		branch: {
+			type: 'string',
+			short: 'b',
+		},
+		'log-level': {
+			type: 'string',
+			default: 'log', // 'log' | 'debug'
+		},
+		'skip-private-repos': {
+			type: 'boolean',
+			default: false
+		},
+		// use only global content sources even if local content sources are present
+		// e.g. for links-checker we should always check in the context of global sources
+		'enforce-global-sources': {
+			type: 'boolean',
+			default: false
+		},
+	};
 	static async getGitData(argValues) {
 		let currentRepoName = argValues.repo;
 		let baseBranchName = argValues.branch;
@@ -32,11 +47,6 @@ class PlaybookLoader {
 		return { currentRepoName, baseBranchName };
 	}
 
-	static async loadLocalAntoraData() {
-		const localAntoraPlaybookContent = fs.readFileSync('./antora-playbook.yml', 'utf8');
-		return YAML.parse(localAntoraPlaybookContent);
-	}
-
 	static async getBranchNameFromPrId(repoName) {
 		const prId = process.env.BRANCH?.replace('pull/', '').replace('/head', '');
 		const response = await fetch(`https://api.github.com/repos/${repoName}/pulls/${prId}`);
@@ -44,179 +54,26 @@ class PlaybookLoader {
 		return prData.base.ref;
 	}
 
-	static removeProtectedSources(sources) {
-		return sources.filter(source =>
-			!(source.url === 'https://github.com/hazelcast/hazelcast-mono')
-			&& !(source.url === 'https://github.com/hazelcast/management-center'));
-	}
-
-	static addHazelcastDocsUrl(sources) {
-		// in the global playbook it's declared with a dot `.`
-		const hazelcastDocsSource = sources.find(source => source.url === '.');
-		hazelcastDocsSource.url = 'https://github.com/hazelcast/hazelcast-docs';
-		hazelcastDocsSource.branches = ['main'];
-		return sources;
-	}
-
-	static rewriteCurrentVersion() {
-		const antoraYmlPath = './docs/antora.yml';
-		try {
-			const antoraYml = YAML.parse(fs.readFileSync(antoraYmlPath, 'utf8'));
-			const version = 'snapshot_ci';
-			antoraYml.version = version;
-			antoraYml.display_version = version;
-			antoraYml.asciidoc.attributes['full-version'] = version;
-			fs.writeFileSync(
-				antoraYmlPath,
-				YAML.stringify(antoraYml),
-				{ encoding: 'utf8' },
-			);
-		} catch (err) {
-			console.debug(err);
-			console.warn('Could not rewrite version. There might be an error with version collision!');
-		}
-	}
-
-	static excludeBaseBranch(sources, repoName, branchName) {
-		// exclude current target branch from the global content list by adding the branch name with the "!" prefix
-		const excludedBranch = `!${branchName}`;
-		const matchedRepos = sources.filter(source => source.url.endsWith(repoName));
-		if (matchedRepos.length === 0) {
-			throw new Error(`There is no repository ${repoName} among the playbook sources!`);
-		}
-
-		const currentSource = PlaybookLoader.getCurrentSource(matchedRepos, branchName);
-		if (Array.isArray(currentSource.branches)) {
-			currentSource.branches.push(excludedBranch);
-		} else {
-			currentSource.branches = [currentSource.branches, excludedBranch];
-		}
-
-		return currentSource;
-	}
-
-	static getCurrentSource(matchedRepos, branchName) {
-		let currentSource = matchedRepos.find(source => {
-			if (Array.isArray(source.branches)) {
-				return source.branches.find(branch => isMatch(branchName, branch));
-			} else {
-				return isMatch(branchName, source.branches);
-			}
-		});
-		if (!currentSource) {
-			console.debug(`No matching base branch found. Rewriting version to omit version collision!`);
-			PlaybookLoader.rewriteCurrentVersion();
-			currentSource = matchedRepos[0];
-		}
-
-		return currentSource;
-	}
-
-	static addCurrentBranch(repoSource, sources) {
-		const currentBranchSource = {
-			url: '.',
-			branches: 'HEAD',
-		};
-
-		// 				- copy start_path(s) from the currentRepoSource
-		if (repoSource.start_path) {
-			currentBranchSource.start_path = repoSource.start_path.slice();
-		}
-		if (repoSource.start_paths) {
-			currentBranchSource.start_paths = repoSource.start_paths.slice();
-		}
-
-		sources.unshift(currentBranchSource);
-	}
-
-	static writeGlobalAntoraPlaybookFile(playbook) {
-		const globalPlaybook = YAML.stringify(playbook);
-
-		console.debug(globalPlaybook);
-
-		fs.writeFileSync(
-			'./global-antora-playbook.yml',
-			globalPlaybook,
-			{ encoding: 'utf8' },
-		);
-	}
-
-	static mergePlaybooks(globalPlaybook, localPlaybook, sources) {
-		return {
-			...globalPlaybook,
-			site: {
-				...globalPlaybook.site,
-				...localPlaybook.site,
-				keys: {
-					...globalPlaybook.site.keys,
-					...(localPlaybook.site?.keys || {}),
-				}
-			},
-			content: {
-				sources: sources,
-			},
-			antora: {
-				extensions: localPlaybook.antora?.extensions || globalPlaybook.antora.extensions,
-			},
-			asciidoc: {
-				attributes: {
-					...globalPlaybook.asciidoc.attributes,
-					...(localPlaybook.asciidoc?.attributes || {}),
-				},
-				extensions: localPlaybook.asciidoc?.extensions || globalPlaybook.asciidoc.extensions,
-			},
-		};
-	}
-
-	constructor() {}
-
 	async loadPlaybook() {
 		const { skipPrivateRepos } = await this.parseInputArgs();
-		const localAntoraPlaybook = await PlaybookLoader.loadLocalAntoraData();
-		const globalAntoraPlaybook = await PlaybookLoader.fetchGlobalAntoraPlaybook();
+		const localAntoraPlaybook = await PlaybookLoaderUtils.loadLocalAntoraData();
+		const globalAntoraPlaybook = await PlaybookLoaderUtils.fetchGlobalAntoraPlaybook();
 		let { contentSources } = this.loadContentSources(globalAntoraPlaybook, localAntoraPlaybook, skipPrivateRepos);
-		const playbook = PlaybookLoader.mergePlaybooks(globalAntoraPlaybook, localAntoraPlaybook, contentSources);
-		// 4. Replace local content.sources with the modified content.sources
-		PlaybookLoader.writeGlobalAntoraPlaybookFile(playbook);
+		const playbook = PlaybookLoaderUtils.mergePlaybooks(globalAntoraPlaybook, localAntoraPlaybook, contentSources);
+		PlaybookLoaderUtils.writeGlobalAntoraPlaybookFile(playbook);
 	}
 
 	async parseInputArgs() {
 		const {
 			values: argValues,
-		} = parseArgs({ options: {
-				repo: {
-					type: 'string',
-					short: 'r',
-				},
-				branch: {
-					type: 'string',
-					short: 'b',
-				},
-				'log-level': {
-					type: 'string',
-					default: 'log', // 'log' | 'debug'
-				},
-				'skip-private-repos': {
-					type: 'boolean',
-					default: false
-				},
-				// use only global content sources even if local content sources are present
-				// e.g. for links-checker we should always check in the context of global sources
-				'enforce-global-sources': {
-					type: 'boolean',
-					default: false
-				},
-			},
-		});
+		} = parseArgs({ options: PlaybookLoader.INPUT_ARGS_OPTIONS });
 
 		const { currentRepoName, baseBranchName } = await PlaybookLoader.getGitData(argValues);
-		const logLevel = argValues['log-level'];
 		const skipPrivateRepos = argValues['skip-private-repos'];
 		const enforceGlobalSources = argValues['enforce-global-sources'];
 
-		if (logLevel !== 'debug') {
-			global.console.debug = () => null;
-		}
+		const logLevel = argValues['log-level'];
+		PlaybookLoaderUtils.setLogLevel(logLevel);
 
 		this.currentRepoName = currentRepoName;
 		this.baseBranchName = baseBranchName;
@@ -231,7 +88,7 @@ class PlaybookLoader {
 		if (this.enforceGlobalSources || !contentSources) {
 			contentSources = globalAntoraPlaybook.content.sources;
 			if (skipPrivateRepos) {
-				contentSources = PlaybookLoader.removeProtectedSources(globalAntoraPlaybook.content.sources);
+				contentSources = PlaybookLoaderUtils.removeProtectedSources(globalAntoraPlaybook.content.sources);
 			}
 			contentSources = this.modifyGlobalContentSources(contentSources);
 		}
@@ -239,9 +96,9 @@ class PlaybookLoader {
 	}
 
 	modifyGlobalContentSources(sources) {
-		sources = PlaybookLoader.addHazelcastDocsUrl(sources);
-		const currentRepoSource = PlaybookLoader.excludeBaseBranch(sources, this.currentRepoName, this.baseBranchName);
-		PlaybookLoader.addCurrentBranch(currentRepoSource, sources);
+		sources = PlaybookLoaderUtils.replaceCurrentRepoWithHazelcastDocsUrl(sources);
+		const currentRepoSource = PlaybookLoaderUtils.excludeBaseBranch(sources, this.currentRepoName, this.baseBranchName);
+		PlaybookLoaderUtils.addCurrentBranchSource(currentRepoSource, sources);
 		return sources;
 	}
 }
---
+++ scripts/playbook-loader-utils.js
@@ -0,0 +1,156 @@
+const YAML = require('yaml');
+const fs = require('fs');
+const {isMatch} = require('matcher');
+
+class PlaybookLoaderUtils {
+	static async fetchGlobalAntoraPlaybook() {
+		const response = await fetch('https://api.github.com/repos/hazelcast/hazelcast-docs/contents/antora-playbook.yml');
+		const playbook = await response.json();
+		const globalAntoraPlaybookContent = Buffer.from(playbook.content, 'base64').toString('utf-8');
+		return YAML.parse(globalAntoraPlaybookContent);
+	}
+
+	static setLogLevel(logLevel) {
+		if (logLevel !== 'debug') {
+			global.console.debug = () => null;
+		}
+	}
+
+	static async loadLocalAntoraData(path = './antora-playbook.yml') {
+		const localAntoraPlaybookContent = fs.readFileSync(path, 'utf8');
+		return YAML.parse(localAntoraPlaybookContent);
+	}
+
+	static removeProtectedSources(sources) {
+		return sources.filter(source =>
+			!(source.url === 'https://github.com/hazelcast/hazelcast-mono')
+			&& !(source.url === 'https://github.com/hazelcast/management-center'));
+	}
+
+	static replaceCurrentRepoWithHazelcastDocsUrl(sources) {
+		// in the global playbook it's declared with a dot `.`
+		const hazelcastDocsSource = sources.find(source => source.url === '.');
+		hazelcastDocsSource.url = 'https://github.com/hazelcast/hazelcast-docs';
+		hazelcastDocsSource.branches = ['main'];
+		return sources;
+	}
+
+	static writeGlobalAntoraPlaybookFile(playbook, path = './global-antora-playbook.yml') {
+		const globalPlaybook = YAML.stringify(playbook);
+
+		console.debug(globalPlaybook);
+
+		fs.writeFileSync(
+			path,
+			globalPlaybook,
+			{ encoding: 'utf8' },
+		);
+	}
+
+	static mergePlaybooks(globalPlaybook, localPlaybook, sources) {
+		return {
+			...globalPlaybook,
+			site: {
+				...globalPlaybook.site,
+				...localPlaybook.site,
+				keys: {
+					...globalPlaybook.site.keys,
+					...(localPlaybook.site?.keys || {}),
+				}
+			},
+			content: {
+				sources: sources,
+			},
+			ui: {
+				...globalPlaybook.ui,
+				...(localPlaybook.ui || {}),
+				bundle: {
+					...globalPlaybook.ui.bundle,
+					...(localPlaybook.ui?.bundle || {}),
+				}
+			},
+			antora: {
+				extensions: localPlaybook.antora?.extensions || globalPlaybook.antora.extensions,
+			},
+			asciidoc: {
+				attributes: {
+					...globalPlaybook.asciidoc.attributes,
+					...(localPlaybook.asciidoc?.attributes || {}),
+				},
+				extensions: localPlaybook.asciidoc?.extensions || globalPlaybook.asciidoc.extensions,
+			},
+		};
+	}
+
+	static addCurrentBranchSource(repoSource, sources) {
+		const currentBranchSource = {
+			url: '.',
+			branches: 'HEAD',
+		};
+
+		if (repoSource.start_path) {
+			currentBranchSource.start_path = repoSource.start_path.slice();
+		}
+		if (repoSource.start_paths) {
+			currentBranchSource.start_paths = repoSource.start_paths.slice();
+		}
+
+		sources.unshift(currentBranchSource);
+	}
+
+	static excludeBaseBranch(sources, repoName, branchName) {
+		// exclude current target branch from the global content list by adding the branch name with the "!" prefix
+		const excludedBranch = `!${branchName}`;
+		const matchedRepos = sources.filter(source => source.url.endsWith(repoName));
+		if (matchedRepos.length === 0) {
+			throw new Error(`There is no repository ${repoName} among the playbook sources!`);
+		}
+
+		const currentSource = PlaybookLoaderUtils.getCurrentSource(matchedRepos, branchName);
+		if (Array.isArray(currentSource.branches)) {
+			currentSource.branches.push(excludedBranch);
+		} else {
+			currentSource.branches = [currentSource.branches, excludedBranch];
+		}
+
+		return currentSource;
+	}
+
+	static rewriteCurrentVersionWithCiVersion() {
+		const antoraYmlPath = './docs/antora.yml';
+		try {
+			const antoraYml = YAML.parse(fs.readFileSync(antoraYmlPath, 'utf8'));
+			const version = 'snapshot_ci';
+			antoraYml.version = version;
+			antoraYml.display_version = version;
+			antoraYml.asciidoc.attributes['full-version'] = version;
+			fs.writeFileSync(
+				antoraYmlPath,
+				YAML.stringify(antoraYml),
+				{ encoding: 'utf8' },
+			);
+		} catch (err) {
+			console.debug(err);
+			console.warn('Could not rewrite version. There might be an error with version collision!');
+		}
+	}
+
+	static getCurrentSource(matchedRepos, branchName) {
+		let currentSource = matchedRepos.find(source => {
+			if (Array.isArray(source.branches)) {
+				return source.branches.find(branch => isMatch(branchName, branch));
+			} else {
+				return isMatch(branchName, source.branches);
+			}
+		});
+		if (!currentSource) {
+			console.debug(`No matching base branch found. Rewriting version to omit version collision!`);
+			PlaybookLoaderUtils.rewriteCurrentVersionWithCiVersion();
+			currentSource = matchedRepos[0];
+		}
+
+		return currentSource;
+	}
+}
+
+module.exports = { PlaybookLoaderUtils };
